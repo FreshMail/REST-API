40c1d0997071b5acb32e0ef316c846160a25be6f
Grzegorz Gorczyca <grzegorz.gorczyca@freshmail.pl>
2016-09-07 14:18:13 +0200

    dodanie obsługi plików

--- class.rest.php
+++ class.rest.php
@@ -3,7 +3,7 @@
 /**
  *  Klasa do uwierzytelniania i wysyłania danych za pomocą REST API FreshMail
  *
- *  @author Tadeusz Kania, Piotr Suszalski
+ *  @author Tadeusz Kania, Piotr Suszalski, Grzegorz Gorczyca
  *  @since  2012-06-14
  *
  */
@@ -20,6 +20,7 @@ class FmRestApi
 
     const host   = 'https://api.freshmail.com/';
     const prefix = 'rest/';
+    const defaultFilePath = '/tmp/';
     //--------------------------------------------------------------------------
 
     /**
@@ -70,7 +71,7 @@ class FmRestApi
     {
         $this->strApiSecret = $strSectret;
         return $this;
-    } // setApiSecret
+    }
 
     public function setContentType( $contentType = '' )
     {
@@ -88,7 +89,7 @@ class FmRestApi
     {
         $this->strApiKey = $strKey;
         return $this;
-    } // setApiKey
+    }
 
     public function doRequest( $strUrl, $arrParams = array(), $boolRawResponse = false )
     {
@@ -112,37 +113,49 @@ class FmRestApi
 
         $resCurl = curl_init( self::host . self::prefix . $strUrl );
         curl_setopt( $resCurl, CURLOPT_HTTPHEADER, $arrHeaders );
-        curl_setopt( $resCurl, CURLOPT_HEADER, false );
+        curl_setopt( $resCurl, CURLOPT_HEADER, true);
         curl_setopt( $resCurl, CURLOPT_RETURNTRANSFER, true);
 
         if ($strPostData) {
             curl_setopt( $resCurl, CURLOPT_POST, true);
             curl_setopt( $resCurl, CURLOPT_POSTFIELDS, $strPostData );
-        } // endif
+        }
 
         $this->rawResponse = curl_exec( $resCurl );
         $this->httpCode = curl_getinfo( $resCurl, CURLINFO_HTTP_CODE );
 
         if ($boolRawResponse) {
             return $this->rawResponse;
-        } // endif
+        }
 
-        $this->response = json_decode( $this->rawResponse, true );
+        $header_size = curl_getinfo($resCurl, CURLINFO_HEADER_SIZE);
+        $header = substr($this->rawResponse, 0, $header_size);
+        $TypePatern = '/Content-Type:\s*([a-z-Z\/]*)\s/';
+        preg_match($TypePatern, $header, $responseType);
+        if(strtolower($responseType[1]) == 'application/zip') {
+            $filePatern = '/filename\=\"([a-zA-Z0-9\.]+)\"/';
+            preg_match($filePatern, $header, $fileName);
+            file_put_contents(self::defaultFilePath.$fileName[1], substr($this->rawResponse, $header_size));
+            $this->response = array('path' =>self::defaultFilePath.$fileName[1]);
+        } else {
+            $this->response = json_decode( substr($this->rawResponse, $header_size), true );
+        }
+
         if ($this->httpCode != 200) {
             $this->errors = $this->response['errors'];
             if (is_array($this->errors)) {
                 foreach ($this->errors as $arrError) {
                     throw new RestException($arrError['message'], $arrError['code']);
-                } // endforeach
-            } // endif
-        } // endif
+                }
+            }
+        }
 
         if (is_array($this->response) == false) {
             throw new Exception('Connection error - curl error message: '.curl_error($resCurl).' ('.curl_errno($resCurl).')');
-        } // endif
+        }
 
         return $this->response;
-    } // doRequest
+    }
 
 }
 
